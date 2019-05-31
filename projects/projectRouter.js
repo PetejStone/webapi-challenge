b178dc0840728a9b063edee18aac13cbb6822411
PetejStone <petejstoney@gmail.com>
2019-05-31 09:49:25 -0700

    created post request for projects

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -14,4 +14,25 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id',(req, res) => {
+    Projects.get(req.params.id)
+    .then(project => {
+        res.status(200).json({project})
+    })
+    .catch(err => {
+        res.status(500).json({message: "Error retrieving project from database"})
+    })
+})
+
+router.post('/', (req,res) => {
+    
+    Projects.insert(req.body)
+    .then(newProject => {
+        res.status(200).json({newProject})
+    })
+    .catch(err => {
+        res.status(500).json({message: "Server cannot add project at this time"})
+    })
+})
+
 module.exports = router
